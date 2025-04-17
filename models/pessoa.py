e06c7ebf71c041b556b85357b5192250dba12f34
Thamires <thamires.sarges.58@gmail.com>
2025-04-16 23:07:10 -0400

    Classe pessoa

---
+++ models/pessoa.py
@@ -0,0 +1,9 @@
+class Pessoa:
+    def __init__(self, nome, telefone, cpf, email, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
+        self.endereco = endereco
+
+        
